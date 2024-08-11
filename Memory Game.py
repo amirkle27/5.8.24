b95f1b392be25537ac205d0bdb16dd5f396b10cf
amirkle27 <amirkle@gmail.com>
2024-08-11 23:49:49 +0300

    Update Memory Game.py

--- Memory Game.py
+++ Memory Game.py
@@ -2,7 +2,6 @@
 def memory_game (i1:str = "", i2:str = "") -> list:
     import random
     letters: list = ['A','A','B','B','C','C','D','D','E','E','F','F']
-    count: int = 12
     while True:
         try:
             ans: str = str(input("Wanna play a new game? (yes/no)").lower())
@@ -15,9 +14,9 @@ def memory_game (i1:str = "", i2:str = "") -> list:
             random.shuffle(letters)
             while letters:
                 try:
-                    i1: int = int(input(f"Please select a number between 1-{count}"))
+                    i1: int = int(input(f"Please select a number between 1-{len(letters)}"))
                     print(f"You chose the letter '{letters[i1-1]}'")
-                    i2: int = int(input(f"Please select another card between 1-{count}"))
+                    i2: int = int(input(f"Please select another card between 1-{len(letters)}"))
                     print(f"You chose the letter '{letters[i2-1]}'")
                     if i1 == i2:
                         print(f"You already chose {i1}. Try something else")
@@ -30,18 +29,9 @@ def memory_game (i1:str = "", i2:str = "") -> list:
                     letters [i1-1], letters [i2-1] = "",""
                     letters.remove("")
                     letters.remove("")
-                    count -= 2
             else:
                 print("Wow! you won!")
-                ans: str = str(input("Wanna play a new game?"))
-                if ans not in ['yes', 'y', 'no', 'n']:
-                    print("Please answer 'yes' or 'no'")
-                    continue
-                elif ans in ['no', 'n']:
-                    print("Sorry to see you go. Goodbye!")
-                    break
                 letters: list = ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F']
-                count: int = 12
         except:
             print("Wrong Key...")
 
